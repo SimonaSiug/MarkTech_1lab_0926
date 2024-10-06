1f329cb6a1ad85632fc4bf796255d8a6d03d2177
SimonaSiug <simonasiugg@gmail.com>
2024-10-06 14:58:58 +0300

    Added btnClick handling code

--- app/src/main/java/com/example/marktech0926/MainActivity.java
+++ app/src/main/java/com/example/marktech0926/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.marktech0926;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvChangeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        final Button button = findViewById(R.id.btnChangeText);
+        tvChangeText = findViewById(R.id.tvChangeText);
+        button.setOnClickListener(v -> tvChangeText.setText("Welcome to android"));
     }
 }
